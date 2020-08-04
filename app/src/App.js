64c1db997848158e5bf9ea16c84a816d819f0be4
Kazunari93 <aooni0930@gmail.com>
2020-08-04 10:33:36 +0900

    Cards component

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-
 import { Cards, Chart, Country } from "./components";
+import { fetchData } from "./api/index";
+import styles from "./App.module.css";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
   render() {
+    const { data } = this.state;
     return (
-      <div className="App">
-        <Cards />
+      <div className={styles.container}>
+        <Cards data={data} />
         <Country />
         <Chart />
       </div>
