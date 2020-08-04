b3ae76a53e2404fd5da0f7894543a45e12fa3ce1
Kazunari93 <aooni0930@gmail.com>
2020-08-04 13:10:22 +0900

    Updates

---
+++ precache-manifest.2c0167c0f6d69ec164f6bf10629583a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5d2664f8c13abc6aed918a6e00ded6",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "4412c4b3bc69a586712e",
+    "url": "/covid-19/static/css/main.28b58a60.chunk.css"
+  },
+  {
+    "revision": "2b7d8b726a9743f529a1",
+    "url": "/covid-19/static/js/2.5d9a5666.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.5d9a5666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4412c4b3bc69a586712e",
+    "url": "/covid-19/static/js/main.df7cdd5c.chunk.js"
+  },
+  {
+    "revision": "4e627d79afcd5e413e1e",
+    "url": "/covid-19/static/js/runtime-main.aec366c4.js"
+  }
+]);
